05a7ea99451961d48887b20f8e6c0e38173a0883
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-03 07:58:20 -0500

    models/fcnet_mc_dropout.py: WIP

---
+++ ronald_bdl/models/fcnet_mc_dropout.py
@@ -0,0 +1,18 @@
+import torch
+
+from .fcnet import FCNet
+
+class FCNetMCDropout(FCNet):
+
+    def __init__(self, input_dim, output_dim, hidden_dim, n_hidden, n_predictions):
+        super().__init__(input_dim, output_dim, hidden_dim, n_hidden)
+
+        self.n_predictions = n_predictions
+
+    def mc_predict(self, x_test):
+        predictions = torch.cat([self.forward(x_test) for _ in range(self.n_predictions)])
+
+        mean = torch.mean(predictions, 0)
+        var = torch.var(predictions, 0)
+
+        return predictions, mean, var
