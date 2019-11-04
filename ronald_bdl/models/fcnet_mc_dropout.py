8b2562e7f8f9ffe31f076dbf378cd1497e63aa31
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-04 14:15:15 -0500

    models/fcnet_mc_dropout.py: RMSE and test log-likelihood

--- ronald_bdl/models/fcnet_mc_dropout.py
+++ ronald_bdl/models/fcnet_mc_dropout.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 from .fcnet import FCNet
 
@@ -11,13 +12,31 @@ class FCNetMCDropout(FCNet):
 
         self.n_predictions = n_predictions
 
-    def mc_predict(self, x_test):
+    def mc_predict(self, X_test, **kwargs):
         # No gradient computation needed for predictions, mean, and var
         # Refer to https://pytorch.org/docs/stable/autograd.html#locally-disable-grad
         with torch.no_grad():
-            predictions = torch.cat([self.forward(x_test) for _ in range(self.n_predictions)])
+            predictions = torch.cat([self.forward(X_test) for _ in range(self.n_predictions)])
 
             mean = torch.mean(predictions, 0)
             var = torch.var(predictions, 0)
 
-        return predictions, mean, var
+            # If y_test is given, calculate RMSE and test log-likelihood
+            metrics = {}
+
+            if 'y_test' in kwargs:
+                y_test = kwargs['y_test']
+                reg_strength = torch.tensor(kwargs['reg_strength'], dtype=torch.float)
+
+                # RMSE
+                metrics['rmse'] = torch.sqrt(torch.mean(torch.pow(y_test - mean, 2)))
+
+                # test log-likelihood
+                metrics['test_ll'] = torch.mean(
+                    torch.logsumexp(- torch.tensor(0.5) * reg_strength * torch.pow(y_test - predictions, 2), 0)
+                    - torch.log(torch.tensor(self.n_predictions, dtype=torch.float))
+                    - torch.tensor(0.5) * torch.log(torch.tensor(2 * np.pi, dtype=torch.float)) 
+                    + torch.tensor(0.5) * torch.log(reg_strength)
+                )
+
+        return predictions, mean, var, metrics
