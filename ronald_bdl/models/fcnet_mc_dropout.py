8260d794da2582cb8c0a66684e6c56c10d28789b
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-08 21:43:09 -0500

    models/fcnet_mc_dropout.py: remove no_grad context from predict_dist()

--- ronald_bdl/models/fcnet_mc_dropout.py
+++ ronald_bdl/models/fcnet_mc_dropout.py
@@ -25,35 +25,41 @@ class FCNetMCDropout(FCNet):
     def predict_dist(self, X_test, n_predictions, **kwargs):
         # No gradient computation needed for predictions, mean, and var
         # Refer to https://pytorch.org/docs/stable/autograd.html#locally-disable-grad
-        with torch.no_grad():
-            # Temporaily disable eval mode
+        #with torch.no_grad():
+
+        was_eval = not self.training
+
+        # Temporaily disable eval mode
+        if was_eval:
             self.train()
-            predictions = torch.stack([self.forward(X_test) for _ in range(n_predictions)])
+        predictions = torch.stack([self.forward(X_test) for _ in range(n_predictions)])
+        
+        if was_eval:
             self.eval()
 
-            mean = torch.mean(predictions, 0)
-            var = torch.var(predictions, 0)
+        mean = torch.mean(predictions, 0)
+        var = torch.var(predictions, 0)
 
-            # If y_test is given, calculate RMSE and test log-likelihood
-            metrics = {}
+        # If y_test is given, calculate RMSE and test log-likelihood
+        metrics = {}
 
-            if 'y_test' in kwargs:
-                y_test = kwargs['y_test']
-                reg_strength = torch.tensor(kwargs['reg_strength'], dtype=torch.float)
+        if 'y_test' in kwargs:
+            y_test = kwargs['y_test']
+            reg_strength = torch.tensor(kwargs['reg_strength'], dtype=torch.float)
 
-                # RMSE
-                metrics['rmse_mc'] = torch.sqrt(torch.mean(torch.pow(y_test - mean, 2)))
+            # RMSE
+            metrics['rmse_mc'] = torch.sqrt(torch.mean(torch.pow(y_test - mean, 2)))
 
-                # RMSE (Non-MC)
-                prediction_non_mc = self.forward(X_test)
-                metrics['rmse_non_mc'] = torch.sqrt(torch.mean(torch.pow(y_test - prediction_non_mc, 2)))
+            # RMSE (Non-MC)
+            prediction_non_mc = self.forward(X_test)
+            metrics['rmse_non_mc'] = torch.sqrt(torch.mean(torch.pow(y_test - prediction_non_mc, 2)))
 
-                # test log-likelihood
-                metrics['test_ll_mc'] = torch.mean(
-                    torch.logsumexp(- torch.tensor(0.5) * reg_strength * torch.pow(y_test[None] - predictions, 2), 0)
-                    - torch.log(torch.tensor(n_predictions, dtype=torch.float))
-                    - torch.tensor(0.5) * torch.log(torch.tensor(2 * np.pi, dtype=torch.float)) 
-                    + torch.tensor(0.5) * torch.log(reg_strength)
-                )
+            # test log-likelihood
+            metrics['test_ll_mc'] = torch.mean(
+                torch.logsumexp(- torch.tensor(0.5) * reg_strength * torch.pow(y_test[None] - predictions, 2), 0)
+                - torch.log(torch.tensor(n_predictions, dtype=torch.float))
+                - torch.tensor(0.5) * torch.log(torch.tensor(2 * np.pi, dtype=torch.float)) 
+                + torch.tensor(0.5) * torch.log(reg_strength)
+            )
 
         return predictions, mean, var, metrics
