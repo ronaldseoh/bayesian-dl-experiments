f4071a008dd665bbdc157263cee2dfaf84288438
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-08 21:43:39 -0500

    models/fcnet_pyro.py: add FCNetPyro to __init__.py

--- ronald_bdl/models/__init__.py
+++ ronald_bdl/models/__init__.py
@@ -1,5 +1,5 @@
 from .fcnet import FCNet
 from .fcnet_mc_dropout import FCNetMCDropout
-from .fcnet_variational_inference import FCNetPyro
+from .fcnet_pyro import FCNetPyro
 
 __all__ = (FCNet, FCNetMCDropout, FCNetPyro)
