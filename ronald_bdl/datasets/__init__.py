c0a8bbf4facf64d83be1b63cfc192a4f7f63ddd3
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-25 09:10:56 -0500

    __init__.py: __all__ should only contain strings

--- ronald_bdl/datasets/__init__.py
+++ ronald_bdl/datasets/__init__.py
@@ -3,7 +3,7 @@ from .uci_datasets import UCIDatasets
 from .toy_datasets import ToyDatasets
 
 __all__ = (
-    MNIST,
-    UCIDatasets,
-    ToyDatasets
+    'MNIST',
+    'UCIDatasets',
+    'ToyDatasets',
 )
--- ronald_bdl/models/__init__.py
+++ ronald_bdl/models/__init__.py
@@ -2,4 +2,9 @@ from .fcnet import FCNet
 from .fcnet_mc_dropout import FCNetMCDropout
 from .fcnet_pyro import FCNetPyro
 
-__all__ = (FCNet, FCNetMCDropout, FCNetPyro)
+__all__ = (
+    'FCNet',
+    'FCNetMCDropout',
+    'FCNetPyro',
+    'squeezenet1_1',
+)
