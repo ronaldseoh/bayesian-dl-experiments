03faf775954fa89220f904739d2d15e99be891ee
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-29 12:55:35 -0500

    models/squeezenet_dropout.py: do what linter says

--- ronald_bdl/models/squeezenet_dropout.py
+++ ronald_bdl/models/squeezenet_dropout.py
@@ -12,7 +12,7 @@ class FireDropout(nn.Module):
         super(Fire, self).__init__()
 
         self.inplanes = inplanes
-        
+
         # Dropout related settings
         if 'dropout_rate' in kwargs:
             self.dropout_rate = kwargs['dropout_rate']
@@ -26,7 +26,7 @@ class FireDropout(nn.Module):
         # Additional dropout layer
         self.squeeze_dropout = create_dropout_layer(
             self.dropout_rate, -1, self.dropout_type,),
-        
+
         self.squeeze_activation = nn.ReLU(inplace=True)
 
         self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
@@ -35,16 +35,16 @@ class FireDropout(nn.Module):
         # Additional dropout layer
         self.expand1x1_dropout = create_dropout_layer(
             self.dropout_rate, -1, self.dropout_type,),
-        
+
         self.expand1x1_activation = nn.ReLU(inplace=True)
 
         self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
                                    kernel_size=3, padding=1)
-        
+
         # Additional dropout layer
         self.expand3x3_dropout = create_dropout_layer(
             self.dropout_rate, -1, self.dropout_type,),
-        
+
         self.expand3x3_activation = nn.ReLU(inplace=True)
 
     def forward(self, x):
@@ -61,7 +61,7 @@ class SqueezeNetDropout(nn.Module):
         super(SqueezeNetDropout, self).__init__()
 
         self.num_classes = num_classes
-        
+
         # Dropout related settings
         if 'dropout_rate' in kwargs:
             self.dropout_rate = kwargs['dropout_rate']
@@ -69,7 +69,7 @@ class SqueezeNetDropout(nn.Module):
         else:
             self.dropout_rate = 0
             self.dropout_type = 'identity'
-            
+   
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=2),
             # Additional dropout layer added here
@@ -91,7 +91,7 @@ class SqueezeNetDropout(nn.Module):
 
         # Final convolution is initialized differently from the rest
         final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
-        
+
         self.classifier = nn.Sequential(
             # SqueezeNet (and many other known CNNs) originally 
             # do have dropout layers here, right before the final output
