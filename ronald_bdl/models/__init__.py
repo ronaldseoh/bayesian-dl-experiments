919316c6767b6752170f5e0279ebd2b00ccabe31
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-29 01:08:51 -0500

    models/__init__.py: added CIFAR10s

--- ronald_bdl/models/__init__.py
+++ ronald_bdl/models/__init__.py
@@ -2,10 +2,14 @@ from .fcnet import FCNet
 from .fcnet_mc_dropout import FCNetMCDropout
 from .fcnet_pyro import FCNetPyro
 from .squeezenet import SqueezeNet
+from .simple_cifar10 import SimpleCIFAR10
+from .simple_cifar10_mc_dropout import SimpleCIFAR10MCDropout
 
 __all__ = (
     FCNet,
     FCNetMCDropout,
     FCNetPyro,
     SqueezeNet,
+    SimpleCIFAR10,
+    SimpleCIFAR10MCDropout,
 )
