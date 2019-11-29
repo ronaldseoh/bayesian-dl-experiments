ac215c7518b8add49632503dd4b5c4ea8ca441cb
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-29 12:52:30 -0500

    models/dropout_custom.py: remove variational dropout references
    it was incomplete, and alternative implementation will take place in near future.

--- ronald_bdl/models/dropout_custom.py
+++ ronald_bdl/models/dropout_custom.py
@@ -1,46 +1,8 @@
 import torch
 import torch.nn as nn
 
-# Ported from
-# https://github.com/j-min/Dropouts/blob/master/Gaussian_Variational_Dropout.ipynb
-class VariationalDropout(nn.Module):
-    def __init__(self, dim, dropout_rate=0.0):
-        super(VariationalDropout, self).__init__()
-
-        self.dim = dim
-        self.max_alpha = (1 - dropout_rate)
-        
-        # Initial alpha
-        self.log_alpha = nn.Parameter(torch.log(torch.ones(dim) * self.max_alpha))
-
-        # N(0,1)
-        self.epsilon = nn.Parameter(torch.randn(dim))
-
-    def kl(self):
-        c1 = 1.16145124
-        c2 = -1.50204118
-        c3 = 0.58629921
-        
-        alpha = self.log_alpha.exp()
-        
-        negative_kl = 0.5 * self.log_alpha + c1 * alpha + c2 * torch.pow(alpha, 2) + c3 * torch.pow(alpha, 3)
-
-        kl = - negative_kl
-        
-        return kl.mean()
-    
-    def forward(self, x):
-        if self.train():
-            # Clip alpha
-            self.log_alpha.data = torch.clamp(self.log_alpha.data, max=self.max_alpha)
-
-            # N(1, alpha)
-            self.epsilon.data = torch.mul(self.epsilon, torch.exp(self.log_alpha))
-
-            return torch.mul(x, self.epsilon)
-        else:
-            return x
-
+# Return a desired type of dropout layer or just identity layer to fit into
+# dropout's places in our network implementations.
 def create_dropout_layer(dropout_rate, dropout_variational_dim=None, dropout_type='identity'):
 
     if dropout_type == 'bernoulli':
