e2439a7320f05578dbdc91be4722f5531a89f71c
Ronald Seoh <ronaldseoh@icloud.com>
2019-11-30 18:20:33 -0500

    models/squeezenet_dropout.py: added predict_dist()

--- ronald_bdl/models/squeezenet_dropout.py
+++ ronald_bdl/models/squeezenet_dropout.py
@@ -116,3 +116,57 @@ class SqueezeNetDropout(nn.Module):
         x = self.features(x)
         x = self.classifier(x)
         return torch.flatten(x, 1)
+
+    def predict_dist(self, test_loader, n_prediction, torch_device):
+
+        was_eval = not self.training
+
+        predictions = []
+        mean_predictions = []
+        metrics = {}
+
+        metrics['accuracy_mc'] = 0
+        metrics['accuracy_non_mc'] = 0
+        metrics['test_ll_mc'] = 0
+
+        with torch.no_grad():
+            for data in test_loader:
+                # Temporaily disable eval mode
+                if was_eval:
+                    self.train()
+
+                inputs, targets = data
+
+                inputs = inputs.to(torch_device)
+                targets = targets.to(torch_device)
+
+                raw_scores_batch = torch.stack(
+                    [self.forward(inputs) for _ in range(n_prediction)])
+
+                predictions_batch = torch.max(raw_scores_batch, 2).values
+
+                mean_raw_scores_batch = torch.mean(raw_scores_batch, 0)
+                mean_predictions_batch = torch.argmax(mean_raw_scores_batch, 1)
+                mean_predictions.append(mean_predictions_batch)
+
+                if was_eval:
+                    self.eval()
+
+                non_mc_raw_scores_batch = self.forward(inputs)
+                non_mc_predictions_batch = torch.argmax(non_mc_raw_scores_batch, 1)
+
+                # Accuracy
+                metrics['accuracy_mc'] += torch.mean((mean_predictions_batch == targets).float())
+                metrics['accuracy_mc'] /= 2
+
+                # Accuracy (Non-MC)
+                metrics['accuracy_non_mc'] += torch.mean((non_mc_predictions_batch == targets).float())
+                metrics['accuracy_non_mc'] /= 2
+
+                # test log-likelihood
+                metrics['test_ll_mc'] -= (F.cross_entropy(mean_raw_scores_batch, targets))
+                metrics['test_ll_mc'] /= 2
+
+            mean_predictions = torch.cat(mean_predictions)
+
+        return predictions, mean_predictions, metrics
